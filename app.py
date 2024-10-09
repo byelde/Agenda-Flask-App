29b50f8cb62458c81d3b7d5a6837215588990350
gabryel-souza <byelacademico@gmail.com>
2024-10-09 13:35:41 -0300

    comments and bugged methods

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ def create_app():
 
     @app.route("/", methods=["GET"])
     def home():
+        # carregar a pagina
         return render_template("views/home.html")
 
 
@@ -13,59 +14,69 @@ def create_app():
     def add():
 
         if request.method == "GET":
+            # carregar a pagina
             return render_template("views/add.html")
     
         else:
+            # recolher o conteudo dos campos
             contact_name    = request.form.get("name")
             contact_surname = request.form.get("surname")
             contact_number  = request.form.get("number")
 
+            # caso algum campo esteja vazio
             if not (contact_name and contact_surname and contact_number):
                 # Flash/Pop-Up
+                # carregar a pagina novamente para ser preenchida
                 return render_template("views/add.html")
             
+            # estruturar um novo contato
             new_contact = {
                 "name": contact_name,
                 "surname": contact_surname,
                 "number": contact_number
             }
-
-            print(new_contact)
-
             # Checar no bd se o numero já está registrado
+            # ...
 
             # Salvar numero no bd
+            #...
 
+            # retornar para a home page
             return redirect(url_for("home"))
 
 
-    @app.route("/edit/<id>", methods=["GET", "POST"]) # Deve receber id
+    @app.route("/edit/<id>", methods=["GET", "POST"]) # ----> Trocar por delete, que, por algum motivo, nao esta funcionando
     def edit(id):
         if request.method == "GET":
+            # carregar a pagina
             return render_template("views/edit.html") # Adicionar contact (como esta no html)
             
         else:
+            # recolher o conteudo dos campos
             contact_name    = request.form.get("name")
             contact_surname = request.form.get("surname")
             contact_number  = request.form.get("number")
 
+            # Caso nenhum campo esteja preenchido
             if not (contact_name or contact_surname or contact_number ):
                 # Flash/Pop-Up
+                # carregar pagina para ser preenchida novamente
                 return render_template("views/edit.html") # Adicionar contact (como esta no html)
 
-            print(contact_name, contact_surname, contact_number)
-
             # Editar contato no banco de dados
+            # ...
 
+            # Direcionar para home page
             return redirect(url_for("home"))
     
 
-    @app.route("/delete/<id>", methods=["GET", "DELETE"]) # Trocar por delete
+    @app.route("/delete/<id>", methods=["GET", "POST"]) # ----> Trocar por delete, que, por algum motivo, nao esta funcionando
     def remove(id):
         if request.method == "DELETE":
-            print("removeu!!")
-            pass
             # remover contato do banco de dados
+            # ...
+            print("removeu")
+            pass
         
         return redirect(url_for("home"))
 
